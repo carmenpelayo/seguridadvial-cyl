95c751af3daa2c99f58baf1017b715d7423dc590
Carmen Pelayo Fernández <90655687+carmenpelayo@users.noreply.github.com>
2024-09-18 10:36:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 #import sklearn
+from mpl_toolkits.basemap import Basemap
 import numpy
 #import plotly.express as px
 #import plotly.graph_objects as go
